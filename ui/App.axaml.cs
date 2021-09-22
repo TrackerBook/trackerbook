861e8dd29347fd52ab2748ff54da6ac41410b364
ea_pyl <>
2021-09-22 20:28:13 +0000

    Add avalonia
    FossilOrigin-Name: 59235c4fc79c57bb6ac7fe7938da1bd6a547a245dee784166af0b25e1a9ae180

---
+++ ui/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using bc_ui.ViewModels;
+using bc_ui.Views;
+
+namespace bc_ui
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
