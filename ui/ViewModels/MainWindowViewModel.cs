579a60e45b9eebc10611ba52c2c2f24715dcf708
ea_pyl <>
2021-09-29 21:18:15 +0000

    Update
    FossilOrigin-Name: 13aab41f45b8e757bdad88136378d74fdc7639669adc5b65b654d9e7b3d1b2ce

--- ui/ViewModels/MainWindowViewModel.cs
+++ ui/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using bc_ui.Models;
+using ReactiveUI;
 
 namespace bc_ui.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            var result = new List<Item>();
+            for (var i = 0; i < 100; i++)
+            {
+                result.Add(new Item { Name = "Name of the book" + i, Path = "C:\\Windows\\Local\\Documents\\test" + i });
+            }
+            Items = new ObservableCollection<Item>(result);
+        }
+
+        private string searchText = string.Empty;
+        public string SearchText
+        {
+            get => searchText;
+            set => this.RaiseAndSetIfChanged(ref searchText, value);
+        }
+
+        private string fileNames = string.Empty;
+        public string FileNames
+        {
+            get => fileNames;
+            set => this.RaiseAndSetIfChanged(ref fileNames, value);
+        }
+
+        public ObservableCollection<Item> Items { get; private set; }
     }
 }
--- ui/Views/MainWindow.axaml.cs
+++ ui/Views/MainWindow.axaml.cs
@@ -1,6 +1,10 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using bc_ui.ViewModels;
 
 namespace bc_ui.Views
 {
@@ -12,11 +16,38 @@ namespace bc_ui.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+            SetupDnd();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
+
+        void SetupDnd()
+        {
+            void DragOver(object sender, DragEventArgs e)
+            {
+                e.DragEffects = e.DragEffects & DragDropEffects.Link;
+
+                if (!e.Data.Contains(DataFormats.FileNames))
+                {
+                    e.DragEffects = DragDropEffects.None;
+                }
+            }
+
+            void Drop(object sender, DragEventArgs e)
+            {
+                if (e.Data.Contains(DataFormats.FileNames))
+                {
+                    ((MainWindowViewModel)this.DataContext).FileNames = string.Join(Environment.NewLine, e.Data.GetFileNames());
+                }
+            }
+
+
+            AddHandler(DragDrop.DropEvent, Drop);
+            AddHandler(DragDrop.DragOverEvent, DragOver);
+        }
+
     }
 }
